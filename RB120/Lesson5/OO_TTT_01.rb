92a31093425a1e9ea4d5033c461adb16cedde5f1
Reksford <rexfordjwoods@yahoo.com>
2019-11-06 15:20:40 -0700

    Additional work and Rubocop

--- RB120/Lesson5/OO_TTT_01.rb
+++ RB120/Lesson5/OO_TTT_01.rb
@@ -1,70 +1,81 @@
 class Board
   WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
-                  [1, 4, 7], [2, 5, 8], [3, 6, 9],
-                  [1, 5, 9], [3, 5, 7]]
+                   [1, 4, 7], [2, 5, 8], [3, 6, 9],
+                   [1, 5, 9], [3, 5, 7]]
 
   def initialize
     @squares = {}
     reset
   end
-  
-  def get_square_at(key)
-    @squares[key]
+
+  def draw
+    puts "     |     |"
+    puts "  #{@squares[1]}  |  #{@squares[2]}  |  #{@squares[3]}"
+    puts "     |     |"
+    puts "-----|-----|-----"
+    puts "     |     |"
+    puts "  #{@squares[4]}  |  #{@squares[5]}  |  #{@squares[6]}"
+    puts "     |     |"
+    puts "-----|-----|-----"
+    puts "     |     |"
+    puts "  #{@squares[7]}  |  #{@squares[8]}  |  #{@squares[9]}"
+    puts "     |     |"
   end
-  
-  def set_square_at(key, marker)
+
+  def []=(key, marker)
     @squares[key].marker = marker
   end
-  
+
   def unmarked_keys
-    @squares.keys.select {|key| @squares[key].unmarked? }
+    @squares.keys.select { |key| @squares[key].unmarked? }
   end
-  
+
   def full?
     unmarked_keys.empty?
   end
-  
+
   def someone_won?
-    !!detect_winner
+    !!winning_marker
   end
-  
-  def count_human_marker(squares)
-    squares.collect(&:marker).count(TTTGame::HUMAN_MARKER)
+
+  def three_identical_markers?(squares)
+    markers = squares.select(&:marked?).collect(&:marker)
+    return false if markers.size != 3
+    markers.min == markers.max
   end
-  
-  def count_computer_marker(squares)
-    squares.collect(&:marker).count(TTTGame::COMPUTER_MARKER)
-  end
-  
-  def detect_winner
+
+  def winning_marker
     WINNING_LINES.each do |line|
-      if count_human_marker(@squares.values_at(*line)) == 3
-        return TTTGame::HUMAN_MARKER
-      elsif count_computer_marker(@squares.values_at(*line)) == 3 
-        return TTTGame::COMPUTER_MARKER
+      squares = @squares.values_at(*line)
+      if three_identical_markers?(squares)
+        return squares.first.marker
       end
     end
     nil
   end
-  
+
   def reset
-    (1..9).each {|key| @squares[key] = Square.new}
+    (1..9).each { |key| @squares[key] = Square.new }
   end
 end
 
 class Square
   INITIAL_MARKER = " "
-  
+
   attr_accessor :marker
 
   def initialize(marker=INITIAL_MARKER)
     @marker = marker
   end
-  
+
   def unmarked?
     marker == INITIAL_MARKER
   end
-  
+
+  def marked?
+    marker != INITIAL_MARKER
+  end
+
   def to_s
     @marker
   end
@@ -81,26 +92,51 @@ end
 class TTTGame
   HUMAN_MARKER = 'X'
   COMPUTER_MARKER = 'O'
+  FIRST_TO_MOVE = HUMAN_MARKER
   attr_reader :board, :human, :computer
 
   def initialize
     @board = Board.new
     @human = Player.new(HUMAN_MARKER)
     @computer = Player.new(COMPUTER_MARKER)
+    @current_turn = FIRST_TO_MOVE
   end
-  
+
+  def play
+    display_welcome_message
+    loop do
+      display_board
+      loop do
+        current_player_moves
+        break if board.someone_won? || board.full?
+        clear_screen_display_board if human_turn?
+      end
+
+      display_result
+      break unless play_again?
+      reset
+    end
+    display_goodbye_message
+  end
+
+  private
+
   def display_welcome_message
     puts "Welcome to Tic-Tac-Toe!"
     puts ""
   end
-  
+
   def display_goodbye_message
     puts "Thanks for playing Tic-Tac-Toe! Goodbye!"
   end
-  
+
+  def clear
+    system 'cls'
+  end
+
   def display_result
-    display_board
-    case board.detect_winner
+    clear_screen_display_board
+    case board.winning_marker
     when HUMAN_MARKER
       puts "You won!"
     when COMPUTER_MARKER
@@ -109,25 +145,23 @@ class TTTGame
       puts "It's a tie!"
     end
   end
-  
-  def display_board(clear=true)
-    system 'cls' if clear
+
+  def display_board
     puts "You are #{human.marker}"
     puts ""
-    puts "     |     |"
-    puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)}"
-    puts "     |     |"
-    puts "-----|-----|-----"
-    puts "     |     |"
-    puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.get_square_at(6)}"
-    puts "     |     |"
-    puts "-----|-----|-----"
-    puts "     |     |"
-    puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
-    puts "     |     |"
+    board.draw
     puts ""
   end
-  
+
+  def clear_screen_display_board
+    clear
+    display_board
+  end
+
+  def human_turn?
+    @current_turn == HUMAN_MARKER
+  end
+
   def human_moves
     puts "Choose a square (#{board.unmarked_keys.join(', ')})"
     square = nil
@@ -136,13 +170,23 @@ class TTTGame
       break if board.unmarked_keys.include?(square)
       puts "Not a valid choice."
     end
-    board.set_square_at(square, human.marker)
+    board[square] = human.marker
   end
-  
+
   def computer_moves
-    board.set_square_at(board.unmarked_keys.sample, computer.marker)
+    board[board.unmarked_keys.sample] = computer.marker
   end
-  
+
+  def current_player_moves
+    if human_turn?
+      human_moves
+      @current_turn = COMPUTER_MARKER
+    else
+      computer_moves
+      @current_turn = HUMAN_MARKER
+    end
+  end
+
   def play_again?
     answer = nil
     loop do
@@ -151,29 +195,16 @@ class TTTGame
       break if %w(y n).include? answer
       puts "Sorry you must answer 'y' or 'n'"
     end
-    
+
     answer == 'y'
   end
 
-  def play
-    display_welcome_message
-    loop do
-      display_board(false)
-      loop do
-        human_moves
-        break if board.someone_won? || board.full?
-        computer_moves
-        display_board
-        break if board.someone_won? || board.full?
-      end
-
-      display_result
-      break unless play_again?
-      board.reset
-      system 'cls'
-      puts "Let's play again!"
-    end
-    display_goodbye_message
+  def reset
+    board.reset
+    @current_turn = FIRST_TO_MOVE
+    clear
+    puts "Let's play again!"
+    puts ""
   end
 end
 
